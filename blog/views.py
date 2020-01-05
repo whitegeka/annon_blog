a2a14ca04235abd71bb086e78ad68e52a5385378
White <white@localhost.localdomain>
2020-01-05 20:31:46 +0300

    Добавлен пагинатор

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import FormView
+from django.core.paginator import Paginator
 
 from .models import Blog, FormBlog
 
@@ -7,15 +8,17 @@ class BlogIndex(FormView):
     form_class = FormBlog
     template_name = 'blog/index.html'
     success_url = '/'
+    paginate = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        list_blog = Blog.objects.filter(state=True).order_by('-created')
+        paginator = Paginator(list_blog, self.paginate)
+        page = self.request.GET.get('page')
+        context['object_list'] = paginator.get_page(page)
+        return context
 
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
 
-    def form_invalid(self, form):
-        return super().form_invalid(form)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['list_message'] = Blog.objects.filter(state=True).order_by('-created')
-        return context
