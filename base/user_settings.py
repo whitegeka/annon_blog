b65289cde2cb602847218248394b11082e1ea476
White <starik3000@ya.ru>
2020-01-07 13:35:15 +0300

    Добавлено описание user_settings.py

--- base/user_settings.py
+++ base/user_settings.py
@@ -1,4 +1,9 @@
 import os
+"""
+Эти настройки разнесены для удобства работы, а так же
+должны быть добавлены в файл .gitignore
+и для того чтоб не мешать настройкам боевого сервера
+"""
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
